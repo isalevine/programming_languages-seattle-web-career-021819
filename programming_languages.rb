dfe4ceec95af31b91db2b8319868e50780657cdf
Isa Levine <isaacdlevine@gmail.com>
2019-01-23 17:02:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,9 @@ def reformat_languages(languages)
   output_hash = {}
   languages.each do |oo_or_func, languages_hash|
     languages_hash.each do |lang_name, type_hash|
-      type_array = type_hash.values
+      type_string = type_hash.values
       output_hash[lang_name] = {}
-      output_hash[lang_name][:type] = type_array
+      output_hash[lang_name][:type] = type_string
       output_hash[lang_name][:style] = oo_or_func
     end
   end
